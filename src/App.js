9b1f5b08503a6137b0c47acfa8ac141196a39634
Daniel Robinson <danielrobinson@Daniels-MacBook-Pro-2.local>
2025-01-18 18:05:55 -0700

    2

--- src/App.js
+++ src/App.js
@@ -6,14 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there!2</p>
+        <p>Hi there Kubs2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Kubernetes
         </a>
       </header>
     </div>
