47c9d4aa58591309ebf4518a68b69776e16413e9
Daniel Robinson <danielrobinson@Daniels-MacBook-Pro-2.local>
2025-02-03 08:49:01 -0700

    updated title

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hi there Kubs2</p>
+        <p>Hi there Kubs3</p>
         <a
           className="App-link"
           href="https://reactjs.org"
